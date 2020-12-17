99dac81073a8eaef940bd1198ec5e324b905b9d7
paul <1311046186@qq.com>
2020-12-17 14:01:04 +0800

    更新

---
+++ testcases/test_add_db_tables.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/17 13:58
+# @Author  : paul
+# @Email   : pp_xiachedan@163.com
+# @File    : test_add_db_tables.py
---
+++ testcases/test_add_sql_tables.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/17 13:59
+# @Author  : paul
+# @Email   : pp_xiachedan@163.com
+# @File    : test_add_sql_tables.py
