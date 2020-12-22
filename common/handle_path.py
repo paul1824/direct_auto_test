48a734e035d97554616ceead2d82eb23da4932d2
paul <1311046186@qq.com>
2020-12-22 17:08:18 +0800

    更新

--- common/handle_path.py
+++ common/handle_path.py
@@ -7,7 +7,7 @@ CONF_DIR = os.path.join(BASE_DIR, 'conf')  # 配置文件目录
 REPORT_DIR = os.path.join(BASE_DIR, 'reports')  # 测试报告路径
 LOG_DIR = os.path.join(BASE_DIR, 'logs')  # 日志目录路径
 CONF_FILE = os.path.join(CONF_DIR, 'config.ini')  # 配置文件路径
-RESULT_FILE = os.path.join(DATA_DIR, 'result_db.ini')  # db数据集结果文件路径
+RESULT_FILE = os.path.join(DATA_DIR, 'db_info.ini')  # db数据集结果文件路径
 if __name__ == '__main__':
     print('项目路径是:', BASE_DIR)
     print('用例脚本保存在：', CASE_DIR)
